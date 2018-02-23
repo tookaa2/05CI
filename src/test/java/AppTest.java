da7b6e78b92bba62fd64a212533174a1cfe5fcb4
Unknown <donutzue@gmail.com>
2018-02-23 15:49:26 +0700

    Travis
    Added travis xml and also created test cases and isPrime metods

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -5,8 +5,24 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class AppTest {
-    @Test public void testAppHasAGreeting() {
-        App classUnderTest = new App();
-        assertNotNull("app should have a greeting", classUnderTest.getGreeting());
+    @Test
+    public void testSmallPrime(){
+        App appObj= new App();
+        assertEquals(appObj.isPrime(17),true);
+    }
+    @Test
+    public void testSmallNoPrime(){
+        App appObj= new App();
+        assertEquals(appObj.isPrime(33),false);
+    }
+    @Test
+    public void testLargePrime(){
+        App appObj= new App();
+        assertEquals(appObj.isPrime(32416189049L),true);
+    }
+    @Test
+    public void testLargeNoPrime(){
+        App appObj= new App();
+        assertEquals(appObj.isPrime(32416189051L),false);
     }
 }
